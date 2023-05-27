cf78adef5dc0c7f651399c2b5a4766349f1c4ae8
Jihye19249205 <119666066+Jihye19249205@users.noreply.github.com>
2023-05-27 10:34:40 +0000

    db修正

--- db/migrate/20230411140429_create_prefectures.rb
+++ db/migrate/20230411140429_create_prefectures.rb
@@ -2,7 +2,7 @@ class CreatePrefectures < ActiveRecord::Migration[6.1]
   def change
     create_table :prefectures do |t|
       #カテゴリID
-      t.references :category, foreign_key: true, null: false
+      t.references :category, null: false
       #都道府県(enum)
       t.string :prefecture, null: false
 
